ef5d18c4690d20c7f732e07f45fd382d3f78b07a
q.lepine <q.lepine@student.isartdigital.com>
2022-04-22 13:53:47 +0200

    edited collision system for entities

--- Assets/Scripts/Entities/Enemies/MobileEnemyMovement.cs
+++ Assets/Scripts/Entities/Enemies/MobileEnemyMovement.cs
@@ -63,9 +63,9 @@ public class MobileEnemyMovement : EntityMovement
 
     override protected void WallDetection()
     {
-        for (int i = 0; i < ray.Count; i++)
+        for (int i = 0; i < _wallRays.Count; i++)
         {
-            Vector3 WallPos = transform.position + new Vector3(0, ray[i], 0);
+            Vector3 WallPos = transform.position + new Vector3(0, _wallRays[i], 0);
             RaycastHit[] tmpHit = Physics.RaycastAll(WallPos, Vector3.right * _direction, _rayWallSize, GroundType, QueryTriggerInteraction.Ignore);
             for (int j = 0; j < tmpHit.Length; j++)
             {
@@ -81,9 +81,9 @@ public class MobileEnemyMovement : EntityMovement
 
     protected override bool DetectWall()
     {
-        for (int i = 0; i < ray.Count; i++)
+        for (int i = 0; i < _wallRays.Count; i++)
         {
-            Vector3 WallPos = transform.position + new Vector3(0, ray[i], 0);
+            Vector3 WallPos = transform.position + new Vector3(0, _wallRays[i], 0);
             RaycastHit[] tmp = Physics.RaycastAll(WallPos, Vector3.right * _direction, _rayWallSize, GroundType, QueryTriggerInteraction.Ignore);
             for (int j = 0; j < tmp.Length; j++)
             {
