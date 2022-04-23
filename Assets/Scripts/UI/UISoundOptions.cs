ddc1e02b3feae74d721f92c48e962641f808db3e
Quentin LEPINE <q.lepine@student.isartdigital.com>
2022-04-23 18:09:48 +0200

    Improved options menu : Improved navigation, sound sliders and values memorisation

--- Assets/Scripts/UI/UISoundOptions.cs
+++ Assets/Scripts/UI/UISoundOptions.cs
@@ -14,19 +14,11 @@ public class UISoundOptions : MonoBehaviour
     [Range(0f, 20f)]
     [SerializeField] private float _maxVolume;
 
-    [Range(-80f, 20f)]
-    [SerializeField] private float _startVolume;
-    [SerializeField]
-    private float _currentVolume;
-
     private void Start()
     {
-        /*
-        _currentVolume = _startVolume;
-        float generalRatio = Mathf.Abs(_minVolume) + Mathf.Abs(_maxVolume);
-        _slider.value = (_currentVolume + Mathf.Abs(_minVolume)) / generalRatio;
-        _mixer.SetFloat(_soundParameter, GetVolume(_slider.value));
-        */
+        float actualValue;
+        _mixer.GetFloat(_soundParameter, out actualValue);
+        _slider.value = GetReverseVolume(actualValue);
     }
 
     public void ChangeVolume()
@@ -40,5 +32,8 @@ public class UISoundOptions : MonoBehaviour
         return Mathf.Abs(_maxVolume) * Mathf.Log10(value);
     }
 
-
+    private float GetReverseVolume(float value)
+    {
+        return Mathf.Pow(10,value/Mathf.Abs(_maxVolume));
+    }
 }
