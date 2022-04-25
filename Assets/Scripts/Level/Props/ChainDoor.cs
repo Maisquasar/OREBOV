2e6b4edddb4e4435556afa9b5cdb825f2f8890b0
Romain BOURGOGNE <r.bourgogne@student.isartdigital.com>
2022-04-25 14:57:59 +0200

    Fix Respawn, Fix Chain door

--- Assets/Scripts/Level/Props/ChainDoor.cs
+++ Assets/Scripts/Level/Props/ChainDoor.cs
@@ -6,20 +6,32 @@ public class ChainDoor : MonoBehaviour
 {
     [SerializeField] bool _isOpen;
     Animator _animator;
-    InteractiveStair _stairs;
+    InteractiveStair[] _stairs;
     // Start is called before the first frame update
     void Start()
     {
-        _stairs = GetComponent<InteractiveStair>();
+        GetStairs();
         _animator = GetComponent<Animator>();
         _animator.SetBool("Open", _isOpen);
-        _stairs.CanBeSelected = _isOpen;
+        SetStairs(_isOpen);
+    }
+
+    void GetStairs()
+    {
+        _stairs = GetComponentsInChildren<InteractiveStair>();
+    }
+
+    void SetStairs(bool value)
+    {
+        foreach (var stair in _stairs)
+            stair.CanBeSelected = value;
+
     }
 
     public void Toggle()
     {
         _isOpen = !_isOpen;
-        _stairs.CanBeSelected = _isOpen;
+        SetStairs(_isOpen);
         _animator.SetBool("Open", _isOpen);
     }
 }
