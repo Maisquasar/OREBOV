1e591021fa13a9b436258f668fdd560281e6cde5
Romain BOURGOGNE <r.bourgogne@student.isartdigital.com>
2022-04-25 13:38:45 +0200

    Fix Stairs to be unusable when door is close

--- Assets/ChainDoor.cs
+++ Assets/ChainDoor.cs
@@ -6,16 +6,20 @@ public class ChainDoor : MonoBehaviour
 {
     [SerializeField] bool _isOpen;
     Animator _animator;
+    InteractiveStair _stairs;
     // Start is called before the first frame update
     void Start()
     {
+        _stairs = GetComponent<InteractiveStair>();
         _animator = GetComponent<Animator>();
         _animator.SetBool("Open", _isOpen);
+        _stairs.CanBeSelected = _isOpen;
     }
 
     public void Toggle()
     {
         _isOpen = !_isOpen;
+        _stairs.CanBeSelected = _isOpen;
         _animator.SetBool("Open", _isOpen);
     }
 }
